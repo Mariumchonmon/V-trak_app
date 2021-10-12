215b18488b9c78e7b436554d575939a43d0f44ab
Mariumchonmon <Mariumakterch@gmail.com>
2021-10-13 02:15:49 +0600

    complete dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';  //for date format
@@ -11,12 +13,22 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   late DateFormat dateFormat;
+
+  // for the boxes
   BoxDecoration _decoration = BoxDecoration(
     shape: BoxShape.rectangle,
     color: Colors.red.shade100,
     borderRadius: BorderRadius.circular(8),
   );
 
+  // for the gridview items
+     ButtonStyle _buttonStyle = ButtonStyle(
+     backgroundColor:  MaterialStateProperty.all<Color>(Colors.blueGrey.shade900,),
+     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+       RoundedRectangleBorder(
+         borderRadius: BorderRadius.circular(10.0),),
+     ),
+ );
   @override
   void initState() {
     super.initState();
@@ -122,96 +134,113 @@ class _DashboardState extends State<Dashboard> {
                     crossAxisCount: 3,
                     children: <Widget>[
                       Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.car_rental, color: Colors.white,),
-                          iconSize: 50,
-                          alignment: Alignment.topCenter,
-                        ),
-
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.list_alt_outlined, color: Colors.white,),
-                          iconSize: 50,
-                        ),),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.map, color: Colors.white,),
-                          iconSize: 50,
+                       padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.car_rental,  color: Colors.white, size: 50,),
+                              SizedBox(height: 8,),
+                              Text("ON-DUTY", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
                         ),
                       ),
                       Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.settings, color: Colors.white,),
-                          iconSize: 50,
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.list_alt,  color: Colors.white, size: 50,),
+                              SizedBox(height: 8,),
+                              Text("JOB LIST", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
                         ),
                       ),
                       Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.map_outlined,  color: Colors.white, size: 50,),
+                              SizedBox(height: 8,),
+                              Text("MAP", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
                         ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.car_rental, color: Colors.white,),
-                          iconSize: 50,
-                        ),),
+                      ),
                       Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.settings,  color: Colors.white, size: 50,),
+                              SizedBox(height: 5,),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text('DEVICE CHECK', style: TextStyle(fontSize: 12),),
+                            )
+                            ],
+                          ),
                         ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.car_rental, color: Colors.white,),
-                          iconSize: 50,
-                        ),),
+                      ),
                       Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              ImageIcon(AssetImage('assets/images/logo_v-trak.png'), size: 70,),
 
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueGrey.shade900,
-                          borderRadius: BorderRadius.circular(10),
+                           //   SizedBox(height: 8,),
+                              Text("VTRAK", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
                         ),
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.message, color: Colors.white, ),
-                          iconSize: 50,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.list_alt,  color: Colors.white, size: 50,),
+                              SizedBox(height: 8,),
+                              Text("ICEMAN", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
                         ),
-
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: ElevatedButton(
+                          onPressed: () {  },
+                          style: _buttonStyle,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.mail,  color: Colors.white, size: 50,),
+                              SizedBox(height: 8,),
+                              Text("MESSAGE", style: TextStyle(fontSize: 12),)
+                            ],
+                          ),
+                        ),
                       ),
                     ],
                   ),
@@ -238,7 +267,23 @@ class _DashboardState extends State<Dashboard> {
                           ),
                          Divider(
                            indent: 30,
-                           height: 1, color: Colors.black,)
+                           height: 1, color: Colors.black,),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.satellite, size: 25,),
+                                SizedBox(width: 10,),
+                                Icon(Icons.directions_car_rounded, size: 25,),
+                                SizedBox(width: 10,),
+                                Icon(Icons.person_pin_outlined, size: 25,),
+                                SizedBox(width: 10,),
+                              ],
+
+                            ),
+                          ),
                         ],
                       ),
                     ),
